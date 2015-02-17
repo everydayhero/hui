d42dc00cf32d1edc6c4a5204094ff57faf78d38a
noweverywhere <marinus@noweverywhere.ca>
2015-02-17 14:01:00 +1000

    Copied holder for graph over. Calling it DataVisualisation until a better name is invented

---
+++ DemoPage/DataVisualisationExample/index.js
@@ -0,0 +1,96 @@
+/** @jsx React.DOM */
+"use strict";
+
+var React     = require('react');
+var Highlight = require('react-highlight');
+var DataVisualisation = require('../../graphs/DataVisualisation');
+
+function getSeries(tick) {
+  var series = [[], [], []];
+  var count = 0;
+
+  while(count < 50) {
+    var value = (Math.abs(Math.sin(count + tick) * 5100000));
+    var value2 = (Math.abs(Math.cos(count + tick) * 5100000));
+    var value3 = (Math.abs(Math.sin(count + 5 + tick) * 3000000));
+
+    series[0].push({
+      date: new Date(2014, 1, count + tick, 0, 0, 0, 0).toISOString(),
+      value: value
+    });
+
+    series[1].push({
+      date: new Date(2014, 1, count + tick, 0, 0, 0, 0).toISOString(),
+      value: value2
+    });
+
+    series[2].push({
+      date: new Date(2014, 1, count + tick, 0, 0, 0, 0).toISOString(),
+      value: value3
+    });
+
+    count++;
+  }
+
+  return series;
+}
+
+
+module.exports = React.createClass({
+  displayName: 'LineGraphExample',
+
+  getInitialState: function() {
+    return {
+      series: getSeries(1),
+      total: 7000,
+      title: 'Example Data'
+    };
+  },
+
+  componentWillMount: function() {
+    var component = this;
+    var tick = 1;
+
+    if(typeof document === 'object') {
+      setInterval(function() {
+        tick++;
+        component.setState({
+          series: getSeries(tick)
+        });
+      }, 100);
+    }
+  },
+
+  render: function() {
+    if (this.state.hide) {
+      return false;
+    }
+
+    return (
+    <div>
+      <h3>LineGraph</h3>
+      <h4>LineGraph propTypes</h4>
+      <ul>
+        <li>series</li>
+        <li>stacked</li>
+        <li>lined</li>
+        <li>gutter
+          <ul>
+            <li>left</li>
+            <li>right</li>
+            <li>bottom</li>
+            <li>top</li>
+          </ul>
+        </li>
+      </ul>
+      <div className="DemoPage__example--graph--lines">
+        <DataVisualisation {...this.state} />
+      </div>
+      <h4>React Example</h4>
+      <Highlight className='html'>
+        { '<LineGraph series={ series } line={ true } area={ false } />' }
+      </Highlight>
+    </div>
+    );
+  }
+});
---
+++ graphs/DataVisualisation/index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var React   = require('react');
+var Graphs  = require('../LineGraph');
+var SingleNumber = require('./SingleNumber');
+
+module.exports = React.createClass({
+  displayName: 'DataVisalisation',
+
+  propTypes: {
+    series: React.PropTypes.array.isRequired,
+    stacked: React.PropTypes.bool,
+    title: React.PropTypes.string.isRequired,
+    total: React.PropTypes.number.isRequired,
+    type: React.PropTypes.oneOf(['donations', 'supporters'])
+  },
+
+  getDefaultProps: function() {
+   return {
+     stacked: true
+   };
+  },
+
+  renderGraph: function() {
+    var series = this.props.series;
+    if (series) {
+      return (
+        <Graphs stacked={ this.props.stacked } series={ series } />
+      );
+    }
+  },
+
+  renderTotal: function() {
+    var total = this.props.total;
+    if (total) {
+      return (
+        <SingleNumber type="donations" title={ this.props.title } value={ total }/>
+      );
+    }
+  },
+
+  render: function() {
+    console.log('XXXXXXXX');
+    return (
+      <div className="DataVisalisation">
+        { this.renderTotal() }
+        { this.renderGraph() }
+      </div>
+    );
+  }
+});
