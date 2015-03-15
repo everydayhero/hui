3cfbb727c413172a737bcbefbb9e823147fb2eb6
Michael Dawson <michaeld@everydayhero.com.au>
2015-03-16 08:41:13 +1000

    refactor arrow background construction

--- graphs/DeltaArrow/index.js
+++ graphs/DeltaArrow/index.js
@@ -8,25 +8,25 @@ module.exports = React.createClass({
   displayName: 'DeltaArrow',
 
   propTypes: {
-    delta: React.PropTypes.number.isRequired,
+    delta: React.PropTypes.number.isRequired
   },
 
   renderTriangle: function() {
-    var delta = this.props.delta;
+    var props = this.props,
+        delta = props.delta,
+        path;
 
     if (delta == null || delta > 0) {
-      return (
-        <svg>
-          <path d="M2.8,38c-1.1,0-1.6-0.8-1-1.7l19.6-34c0.6-1,1.5-1,2,0l19.6,34c0.5,1,0.1,1.7-1,1.7H2.8z" />
-        </svg>
-      );
+      path = "M2.8,38c-1.1,0-1.6-0.8-1-1.7l19.6-34c0.6-1,1.5-1,2,0l19.6,34c0.5,1,0.1,1.7-1,1.7H2.8z";
     } else {
-      return (
-        <svg>
-          <path d="M42.1,1.3c1.1,0,1.5,0.8,1,1.7L23.5,37c-0.5,1-1.5,1-2,0L1.8,3c-0.5-1-0.1-1.7,1-1.7H42.1z" />
-        </svg>
-      );
+      path = "M42.1,1.3c1.1,0,1.5,0.8,1,1.7L23.5,37c-0.5,1-1.5,1-2,0L1.8,3c-0.5-1-0.1-1.7,1-1.7H42.1z";
     }
+
+    return (
+      <svg>
+        <path d={ path } />
+      </svg>
+    );
   },
 
   render: function() {
