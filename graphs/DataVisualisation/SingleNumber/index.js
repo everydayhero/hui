e6697cdcae26f9a2a14912dee191fcbfb74ba9c6
Michael Dawson <michaeld@everydayhero.com.au>
2015-03-16 14:18:34 +1000

    loading support for single number

--- graphs/DataVisualisation/SingleNumber/index.js
+++ graphs/DataVisualisation/SingleNumber/index.js
@@ -6,23 +6,43 @@ var numeral = require('numeral');
 module.exports = React.createClass({
   displayName: 'SingleNumber',
 
+  propTypes: {
+    loading: React.PropTypes.bool
+  },
+
   getDefaultProps: function() {
     return {
       format: '0.0a',
+      loading: false
     };
   },
 
   metricFormatted: function() {
-    var props = this.props;
+    var props   = this.props,
+        value   = props.value,
+        format  = props.format,
+        loading = props.loading;
 
-    return numeral(props.value).format(props.format);
+    if (loading) {
+      return '&nbsp;';
+    } else {
+      return numeral(value).format(format);
+    }
   },
 
   render: function() {
+    var loading = this.props.loading,
+        className;
+
+    if (loading) {
+      className = "hui-SingleNumber--loading";
+    } else {
+      className = "hui-SingleNumber";
+    }
+
     return (
-      <div className="hui-SingleNumber">
-        <div className="hui-SingleNumber__metric">
-          { this.metricFormatted() }
+      <div className={ className }>
+        <div className="hui-SingleNumber__metric" dangerouslySetInnerHTML={{ __html: this.metricFormatted() }}>
         </div>
       </div>
     );
