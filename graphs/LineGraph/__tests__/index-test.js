e63348a6c330c7d4b2113639787f77579f1476b8
Michael Dawson <michaeld@everydayhero.com.au>
2015-03-17 15:17:17 +1000

    correct indentation

--- graphs/LineGraph/__tests__/index-test.js
+++ graphs/LineGraph/__tests__/index-test.js
@@ -8,23 +8,23 @@ describe('LineGraph', function() {
   var TestUtils = React.addons.TestUtils;
 
   var series = [
-        [
-          { date: new Date("2014, 1").toISOString(), value: 5 },
-          { date: new Date("2014, 2").toISOString(), value: 22 },
-          { date: new Date("2014, 3").toISOString(), value: 14 },
-          { date: new Date("2014, 4").toISOString(), value: 5 },
-          { date: new Date("2014, 5").toISOString(), value: 10 },
-          { date: new Date("2014, 6").toISOString(), value: 24 }
-        ],
-        [
-          { date: new Date("2014, 1").toISOString(), value: 4 },
-          { date: new Date("2014, 2").toISOString(), value: 6 },
-          { date: new Date("2014, 3").toISOString(), value: 11 },
-          { date: new Date("2014, 4").toISOString(), value: 2 },
-          { date: new Date("2014, 5").toISOString(), value: 4 },
-          { date: new Date("2014, 6").toISOString(), value: 23 }
-        ]
-      ];
+    [
+      { date: new Date("2014, 1").toISOString(), value: 5 },
+      { date: new Date("2014, 2").toISOString(), value: 22 },
+      { date: new Date("2014, 3").toISOString(), value: 14 },
+      { date: new Date("2014, 4").toISOString(), value: 5 },
+      { date: new Date("2014, 5").toISOString(), value: 10 },
+      { date: new Date("2014, 6").toISOString(), value: 24 }
+    ],
+    [
+      { date: new Date("2014, 1").toISOString(), value: 4 },
+      { date: new Date("2014, 2").toISOString(), value: 6 },
+      { date: new Date("2014, 3").toISOString(), value: 11 },
+      { date: new Date("2014, 4").toISOString(), value: 2 },
+      { date: new Date("2014, 5").toISOString(), value: 4 },
+      { date: new Date("2014, 6").toISOString(), value: 23 }
+    ]
+  ];
 
   describe('default', function() {
     var component;
