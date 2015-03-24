b83808ab19078597fe12d62c36469d1126b5aa6a
Michael Dawson <michaeld@everydayhero.com.au>
2015-03-24 15:26:16 +1000

    add additional example legend items

--- DemoPage/examples/Legend/index.js
+++ DemoPage/examples/Legend/index.js
@@ -21,7 +21,7 @@ module.exports = React.createClass({
       <div>
         <h3 className="DemoPage__h3">Legend</h3>
         <div className="DemoPage__example--legend">
-          <Legend labels={[ 'Campaign 1', 'Campaign 2', 'Campaign 3' ]} />
+          <Legend labels={[ 'Campaign 1', 'Campaign 2', 'Campaign 3', 'Campaign 4', 'Campaign 5', 'Campaign 6', 'Campaign 7' ]} />
         </div>
 
         <h4 className="DemoPage__h4">React Example</h4>
