b7b32f5e80b88c2a39c7becbb89273725a46c58a
Mbuckley0 <m5.buckley@connect.qut.edu.au>
2015-05-21 16:43:12 +1000

    Fixed up Icon and Separator issues

--- layout/PageForm/index.js
+++ layout/PageForm/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var React      = require('react');
-var Icon       = require('./Icon');
+var Icon       = require('../../Helpers/Icon');
 var Separator  = require('./Separator');
 
 module.exports = React.createClass({
