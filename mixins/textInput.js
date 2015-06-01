d0783ded62b2a7bfa9a07ff77e58573bbee5f610
David Herse <davidh@everydayhero.com.au>
2015-06-01 11:26:36 +1000

    fix date input form submit bug

--- mixins/textInput.js
+++ mixins/textInput.js
@@ -119,9 +119,9 @@ module.exports = {
 
     if (props.onIconClick) {
       return (
-        <button className={ className } onClick={ props.onIconClick }>
+        <a href="" className={ className } onClick={ props.onIconClick }>
           <Icon icon={ icon } fixedWidth={ true } />
-        </button>
+        </a>
       );
     }
     return (
