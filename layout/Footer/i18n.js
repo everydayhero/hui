8f639cc71c5b5a3cd8bf24a6a0a24ec1af1605c8
Mbuckley0 <m5.buckley@connect.qut.edu.au>
2015-07-03 13:28:35 +1000

    Fix links for the footer

--- layout/Footer/i18n.js
+++ layout/Footer/i18n.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var urls = require('../../urls');
-var portalUrl = urls.getUrl('portal');
 
 module.exports = {
   en: {
@@ -9,27 +8,27 @@ module.exports = {
     left_2: 'Jobs',
     left_3: 'Wonderwall',
     left_4: 'Contact',
-    left_1_url: portalUrl + 'about/',
-    left_2_url: portalUrl + 'jobs/',
-    left_3_url: portalUrl + 'blog/',
-    left_4_url: portalUrl + 'contact/',
+    left_1_url: '{portal}about/',
+    left_2_url: '{portal}jobs/',
+    left_3_url: '{portal}blog/',
+    left_4_url: '{portal}contact/',
 
     right_1: 'Press',
     right_2: 'Events',
     right_3: 'Nonprofits',
     right_4: 'Developers',
-    right_1_url: portalUrl + 'press/',
-    right_2_url: portalUrl + 'events/',
-    right_3_url: portalUrl + 'nonprofits/',
-    right_4_url: portalUrl + 'developers-giving-ecosystem/',
+    right_1_url: '{portal}press/',
+    right_2_url: '{portal}events/',
+    right_3_url: '{portal}nonprofits/',
+    right_4_url: '{portal}developers-giving-ecosystem/',
 
     legal_1: 'Privacy Policy',
     legal_2: 'Terms of Service',
     legal_3: 'Cookie Policy',
     legal_4: 'everydayhero is a Blackbaud service',
-    legal_1_url: portalUrl + 'terms/privacy',
-    legal_2_url: portalUrl + 'terms/',
-    legal_3_url: portalUrl + 'terms/cookies',
+    legal_1_url: '{portal}terms/privacy',
+    legal_2_url: '{portal}terms/',
+    legal_3_url: '{portal}terms/cookies',
     legal_4_url: "https://www.blackbaud.com/",
 
     registration_number: 'EIN:',
@@ -43,22 +42,22 @@ module.exports = {
     left_2: 'Charities',
     left_3: 'Schools',
     left_4: 'Events',
-    left_1_url: portalUrl + 'about/',
-    left_2_url: portalUrl + 'charities/',
-    left_3_url: portalUrl + 'schools/',
-    left_4_url: portalUrl + 'fundraising-events-to-participate-in/',
+    left_1_url: '{portal}about/',
+    left_2_url: '{portal}charities/',
+    left_3_url: '{portal}schools/',
+    left_4_url: '{portal}fundraising-events-to-participate-in/',
 
     right_1: 'Wonderwall',
     right_2: 'Blog',
     right_3: 'Jobs',
     right_4: 'Contact',
-    right_1_url: portalUrl + 'blog/',
+    right_1_url: '{portal}blog/',
     right_2_url: 'http://everydayherocommunity.com/',
-    right_3_url: portalUrl + 'jobs/',
-    right_4_url: portalUrl + 'contact/',
+    right_3_url: '{portal}jobs/',
+    right_4_url: '{portal}contact/',
 
     legal_3: 'Ethics Policy',
-    legal_3_url: portalUrl + 'ethics_policy',
+    legal_3_url: '{portal}ethics_policy',
 
     registration_number: 'ABN:'
   },
@@ -67,22 +66,22 @@ module.exports = {
     left_2: 'Charities',
     left_3: 'Schools',
     left_4: 'Events',
-    left_1_url: portalUrl + 'about/',
-    left_2_url: portalUrl + 'charities/',
-    left_3_url: portalUrl + 'schools/',
-    left_4_url: portalUrl + 'fundraising-events-to-participate-in/',
+    left_1_url: '{portal}about/',
+    left_2_url: '{portal}charities/',
+    left_3_url: '{portal}schools/',
+    left_4_url: '{portal}fundraising-events-to-participate-in/',
 
     right_1: 'Wonderwall',
     right_2: 'Blog',
     right_3: 'Jobs',
     right_4: 'Contact',
-    right_1_url: portalUrl + 'blog/',
+    right_1_url: '{portal}blog/',
     right_2_url: 'http://everydayherocommunity.com/',
-    right_3_url: portalUrl + 'jobs/',
-    right_4_url: portalUrl + 'contact/',
+    right_3_url: '{portal}jobs/',
+    right_4_url: '{portal}contact/',
 
     legal_3: 'Ethics Policy',
-    legal_3_url: portalUrl + 'ethics_policy',
+    legal_3_url: '{portal}ethics_policy',
 
     registration_number: 'IRD:'
   },
