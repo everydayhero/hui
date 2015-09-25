668e925727e7bae5fecfc2dc58954f94321c7ec7
Brad Parker <hi@bradparker.com>
2015-09-25 16:12:06 +1000

    Adds FlagIcon spec

--- atoms/FlagIcon/__tests__/FlagIcon-test.js
+++ atoms/FlagIcon/__tests__/FlagIcon-test.js
@@ -1,6 +1,11 @@
 'use strict';
 
-describe('FlagIcon', function () {
-  it('write some expectations')
-})
+var FlagIcon = require('../')
 
+describe('FlagIcon', function () {
+  it('renders a flag icon', function() {
+    var element = renderIntoDocument(<FlagIcon country="AU"/>);
+    var iconClass = element.getDOMNode().children[0].className;
+    expect(iconClass).to.eq('flag flag-au');
+  });
+});
