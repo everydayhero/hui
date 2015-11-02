9f42db87b1ceec56c7d3968064c73dde5d0d8566
Brad Parker <hi@bradparker.com>
2015-11-02 16:01:45 +1000

    Uses TextInput props module in TextInput
    This one's dicy.
    I think other components might need that large list of options to pass
    to children, however it makes the component file way less descriptive...

--- forms/TextInput/index.js
+++ forms/TextInput/index.js
@@ -4,6 +4,7 @@ import React from 'react/addons'
 import LocalStorageMixin from '../../mixins/localStorage'
 import inputMessage from '../../mixins/inputMessage'
 import textInput from '../../mixins/textInput'
+import { types, defaults } from '../../mixins/textInputProps'
 import classnames from 'classnames'
 
 export default React.createClass({
@@ -11,83 +12,10 @@ export default React.createClass({
 
   mixins: [LocalStorageMixin, inputMessage, textInput],
 
-  propTypes: {
-    id: React.PropTypes.string,
-    name: React.PropTypes.string,
-    type: React.PropTypes.oneOf(['text', 'number']),
-    value: React.PropTypes.string,
-    label: React.PropTypes.string,
-    placeHolder: React.PropTypes.string,
-    className: React.PropTypes.string,
-    errorMessage: React.PropTypes.string,
-    hint: React.PropTypes.string,
-    icon: React.PropTypes.string,
-    iconPosition: React.PropTypes.string,
-    spacing: React.PropTypes.oneOf(['compact', 'tight', 'fitted', 'loose']),
-    layout: React.PropTypes.oneOf(['full', 'wide', 'twoThirds', 'half', 'narrow', 'quarter', 'threeQuarters', 'eighth', 'sevenEighths', 'third']),
-
-    autoComplete: React.PropTypes.bool,
-    storeLocally: React.PropTypes.bool,
-    autoFocus: React.PropTypes.bool,
-    readOnly: React.PropTypes.bool,
-    disabled: React.PropTypes.bool,
-    showError: React.PropTypes.bool,
-    required: React.PropTypes.bool,
-    showIcon: React.PropTypes.bool,
-
-    errors: React.PropTypes.array,
-
-    limit: React.PropTypes.number,
-
-    mask: React.PropTypes.func,
-    onFocus: React.PropTypes.func,
-    onChange: React.PropTypes.func,
-    validate: React.PropTypes.func,
-    onError: React.PropTypes.func,
-    onBlur: React.PropTypes.func,
-    onTab: React.PropTypes.func,
-    onKeyDown: React.PropTypes.func,
-    onIconClick: React.PropTypes.func
-  },
+  propTypes: types,
 
   getDefaultProps() {
-    return {
-      id: '',
-      name: '',
-      type: 'text',
-      value: '',
-      label: 'Input',
-      placeHolder: '',
-      className: '',
-      errorMessage: '',
-      hint: '',
-      icon: '',
-      iconPosition: 'right',
-      spacing: 'loose',
-      layout: 'full',
-
-      autoComplete: true,
-      storeLocally: false,
-      autoFocus: false,
-      disabled: false,
-      showError: false,
-      required: false,
-      showIcon: true,
-
-      errors: [],
-
-      limit: 0,
-
-      mask: str => str,
-      onFocus: () => {},
-      onChange: () => {},
-      validate: null,
-      onError: () => {},
-      onBlur: () => {},
-      onTab: () => {},
-      onKeyDown: () => {},
-      onIconClick: () => {}
-    }
+    return defaults
   },
 
   getInitialState() {
