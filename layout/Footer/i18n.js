dbc8ce687f3a5f4e84bfadd34aaeb132f78d5f08
Sam Enoka <slwenoka@gmail.com>
2016-04-12 17:44:31 +1000

    Update US link names and URLs

--- layout/Footer/i18n.js
+++ layout/Footer/i18n.js
@@ -86,7 +86,12 @@ module.exports = {
   us: {
     registration_number: 'EIN:',
     left_3: 'Blog',
-    right_3_url: '{portal}nonprofits/'
+    right_2: 'Events',
+    right_3: 'Nonprofits',
+    right_4: 'Developers',
+    right_2_url: '{portal}fundraising-events-to-participate-in/',
+    right_3_url: '{portal}nonprofits/',
+    right_4_url: '{portal}developers-giving-ecosystem/'
   },
   ie: {
     registration_number: 'VAT:',
