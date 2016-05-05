96819ce2d747b2e7fd9ca6ca5ed2d0395ff7336c
Ben O'Neal <benoneal@Odin.local>
2016-05-05 13:03:51 +1000

    Add tests

---
+++ forms/PhoneInput/__tests__/PhoneInput-test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import PhoneInput from '../'
+
+describe('PhoneInput', () => {
+  describe('initialisation', () => {
+    it('sets dial code to the default country (Australia)', () => {
+      const { value, dial_code } = renderIntoDocument(<PhoneInput />).state.selectedCountry
+      value.should.eql('AU')
+      dial_code.should.eql('+61')
+    })
+
+    it('sets dial code to a provided default country', () => {
+      const { value, dial_code } = renderIntoDocument(<PhoneInput countryCode="gb" />).state.selectedCountry
+      value.should.eql('GB')
+      dial_code.should.eql('+44')
+    })
+  })
+
+  it('allows country to be changed', (done) => {
+    const subject = renderIntoDocument(<PhoneInput />)
+    Simulate.click(findByClass(subject, 'hui-FilterSelect__display-input'))
+    Simulate.mouseDown(scryByClass(subject, 'hui-OptionListItem__radio-label')[5])
+    Simulate.blur(scryByClass(subject, 'hui-OptionListItem__radio--hidden')[5])
+
+    setTimeout(() => {
+      subject.state.isSelectingCountry.should.eq(false)
+      subject.state.selectedCountry.value.should.eql('US')
+      subject.state.selectedCountry.dial_code.should.eql('+1')
+      done()
+    }, 1)
+  })
+})
