cc587e59dc212cedc8c0d647072bb16a4c37a531
Justin Marrington <justin.marrington@everydayhero.com>
2016-07-20 22:25:56 +1000

    CSS for Donation Amounts

--- forms/CustomDonationAmount/index.js
+++ forms/CustomDonationAmount/index.js
@@ -1,35 +1,45 @@
 import React from 'react'
+import classNames from 'classnames'
 
 const CustomDonationAmount = ({
   handleChanged,
+  handleFocused,
   amount,
   selected,
   currency
 }) => (
   <li
-    className='string required'
-    id='donation_donation_builder_defined_amount_input'
+    className='donation-preset'
   >
-    <label for='donation_donation_builder_defined_amount'>Other amount</label>
-    <span className='custom-donation currency-symbol'>{currency.symbol}</span>
-    <input
-      className='surcharge-input'
-      id='donation_donation_builder_defined_amount'
-      name='defined_amount'
-      type='number'
-      value={amount > 0 ? amount / 100 : ''}
-      onChange={function (event) {
-        const centsValue = parseFloat(event.target.value) * 100
-        try {
-          handleChanged(centsValue)
-        } catch (e) {}
-      }}
-    />
+    <div
+      id="donation_donation_builder_defined_amount_input"
+      className={classNames({
+        'donation-preset-amount-label': true,
+        selected
+      })}
+    >
+      <label htmlFor='donation_donation_builder_defined_amount'>Other amount:</label>
+      <input
+        className='surcharge-input'
+        id='donation_donation_builder_defined_amount'
+        name='defined_amount'
+        type='number'
+        value={amount > 0 ? amount / 100 : ''}
+        onChange={function (event) {
+          const centsValue = parseFloat(event.target.value) * 100
+          try {
+            handleChanged(centsValue)
+          } catch (e) {}
+        }}
+        onFocus={function () { handleFocused() }}
+      />
+    </div>
   </li>
 )
 
 CustomDonationAmount.propTypes = {
   handleChanged: React.PropTypes.func.isRequired,
+  handleFocused: React.PropTypes.func,
   amount: React.PropTypes.number.isRequired,
   selected: React.PropTypes.bool,
   currency: React.PropTypes.shape({
--- forms/DonationAmount/index.js
+++ forms/DonationAmount/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames'
 
 import numeric from '../../lib/numeric'
 
@@ -8,20 +9,28 @@ const DonationAmount = ({
   selected,
   handleSelected
 }) => {
-  const id = `donation_donation_builder_predefined_amount_${amount.cents}`
+  const id = `donation_donation_builder_predefined_amount_${amount}`
   return (
-    <label for={id}>
-      <input
-        id={id}
-        className='surcharge-input'
-        name='predefined_amount'
-        type='radio'
-        value={amount}
-        checked={selected}
-        onChange={handleSelected}
-      />
-      {numeric.money(currency.symbol, amount, '0,0')}
-    </label>
+    <li className='donation-preset'>
+      <label
+        htmlFor={id}
+        className={classNames({
+          'donation-preset-amount-label': true,
+          selected
+        })}
+      >
+        <input
+          id={id}
+          className='donation-preset-amount'
+          name='predefined_amount'
+          type='radio'
+          value={amount}
+          checked={selected}
+          onChange={handleSelected}
+        />
+        {numeric.money('', amount, '0,0')}
+      </label>
+    </li>
   )
 }
 
