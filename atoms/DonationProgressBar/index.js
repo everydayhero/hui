deb9a3424fd771e1493a60b9f77f0fc31b1597a1
Justin Marrington <justin.marrington@everydayhero.com>
2016-07-22 11:51:17 +1000

    Customise value of DonationProgressBar if donor exceeds goal

--- atoms/DonationProgressBar/index.js
+++ atoms/DonationProgressBar/index.js
@@ -3,21 +3,15 @@ import React from 'react'
 import DonationProgressElement from '../DonationProgressElement'
 import numeric from '../../lib/numeric'
 
-const DonationProgressBar = ({
-  fundraiserName,
-  currency,
-  donationTarget,
-  currentTotal,
-  personalInfluence
-}) => {
-  const remaining = donationTarget - currentTotal - personalInfluence
-  const remainingCurrency = numeric.money(currency.symbol, remaining, '0,0')
+const DonationProgressBar = (props) => {
+  const targetLabel = getProgressLabel(props)
+  const { currency, donationTarget, currentTotal, personalInfluence } = props
   const targetCurrency = numeric.money(currency.symbol, donationTarget, '0,0')
 
   return (
     <div className='donation-progress-container'>
       <div className='donation-progress-caption donation-progress-remaining'>
-        {`${fundraiserName} has ${remainingCurrency} to go`}
+        {targetLabel}
       </div>
 
       <div className='donation-progress background'>
@@ -50,6 +44,24 @@ DonationProgressBar.propTypes = {
   donationTarget: React.PropTypes.number.isRequired,
   currentTotal: React.PropTypes.number.isRequired,
   personalInfluence: React.PropTypes.number
+}
+
+const getProgressLabel = ({
+  currentTotal,
+  donationTarget,
+  personalInfluence,
+  currency,
+  fundraiserName
+}) => {
+  const remaining = donationTarget - currentTotal - personalInfluence
+  const remainingCurrency = numeric.money(currency.symbol, Math.abs(remaining), '0,0')
+  if (remaining > 0) {
+    return `${fundraiserName} has ${remainingCurrency} to go`
+  } else if (remaining === 0) {
+    return `Fantastic! ${fundraiserName} has met their goal`
+  } else {
+    return `Fantastic! ${fundraiserName} is ${remainingCurrency} past their goal`
+  }
 }
 
 export default DonationProgressBar
