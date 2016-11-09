393ecce74cf3a5b990fbad15c6f13fdbd4c65f25
Sam Enoka <slwenoka@gmail.com>
2016-11-09 12:00:18 +1000

    Ignore transpiled code when linting (#416)

--- gulp_tasks/lint.js
+++ gulp_tasks/lint.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var standard = require('gulp-standard')
 
 gulp.task('lint', function () {
-  return gulp.src(['./**/*.js', '!./node_modules/**', '!./bin/**', '!./gulp*', '!./gulp_tasks/**', '!./dist/**', '!./**/*Shim*'])
+  return gulp.src(['./**/*.js', '!./node_modules/**', '!./bin/**', '!./gulp*', '!./gulp_tasks/**', '!./dist/**', '!./transpiled/**', '!./**/*Shim*'])
     .pipe(standard())
     .pipe(standard.reporter('default', {
       breakOnError: false,
